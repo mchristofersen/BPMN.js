6830dc392256aeca09b642f36362560554a51202
source_server <source_server@example.com>
2016-06-22 11:46:27 +0000

    modify workspace

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,7 +181,7 @@ module.exports = function(grunt) {
     connect: {
       options: {
         appName: 'chromium-browser', // name of the app that opens, ie: open, start, xdg-open
-        port: 9013,
+        port: 80,
         livereload: 9014,
         hostname: 'localhost'
       },
